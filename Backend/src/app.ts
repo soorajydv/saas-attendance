e38eebb2ae675ee6deb238c48d0773f7fa33e7ae
soorajydv <soorajydv9@gmail.com>
2025-04-11 14:31:39 +0545

    added cors origin of new vercel frontend domain

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,7 +11,7 @@ export const createServer = (): http.Server => {
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['https://saas-attendance-frontend.vercel.app', 'http://localhost:5000'], // Allowed frontend URLs
+  origin: ['https://mark01-frontend.vercel.app', 'http://localhost:5000'], // Allowed frontend URLs
   credentials: true, // Allow cookies and authentication headers
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers
@@ -54,42 +54,6 @@ app.use((req:Request, res:Response, next:NextFunction) => {
 
   // Logging middleware
   app.use(morgan('dev'));
-
-  // app.use((req, res, next) => {
-  //   const start = Date.now();
-  //   // Capture the response status and log after the response is finished
-  //   res.on('finish', () => {
-  //     const duration = Date.now() - start;
-
-  //     // Define status code descriptions
-  //     // let statusColor;
-  //     let statusDescription = '';
-      
-  //     // if (res.statusCode >= 500) {
-  //     //   statusColor = chalk.red; // Server error (500-599)
-  //     //   statusDescription = 'Internal Server Error';
-  //     // } else if (res.statusCode >= 400) {
-  //     //   statusColor = chalk.yellow; // Client error (400-499)
-  //     //   statusDescription = 'Client Error';
-  //     // } else if (res.statusCode >= 300) {
-  //     //   statusColor = chalk.cyan; // Redirection (300-399)
-  //     //   statusDescription = 'Redirection';
-  //     // } else if (res.statusCode >= 200) {
-  //     //   statusColor = chalk.green; // Success (200-299)
-  //     //   statusDescription = 'Success';
-  //     // } else {
-  //     //   statusColor = chalk.white; // Default for other cases (informational)
-  //     //   statusDescription = 'Informational Response';
-  //     // }
-
-  //     // Log the request details along with the colored status code and description
-  //     console.log(
-  //       `${req.method} ${req.originalUrl} ${res.statusCode} - ${statusDescription} - ${duration}ms`
-  //     );
-  //   });
-
-  //   next();
-  // });
 
   // Rate limiter middleware
   app.use('/api', apiLimiter);
